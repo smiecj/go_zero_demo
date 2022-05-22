c195aa3fe30f55af24c0a7a658cf863a875ea15a
smiecj <459559238@qq.com>
2022-05-22 22:39:58 +0800

    init

---
+++ greet/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"github.com/smiecj/go_zero_demo/greet/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
